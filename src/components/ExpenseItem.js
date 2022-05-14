c415a3bfc907aa94e77c06d944ab6da8872b0d70
Kristijan Federer <federer.kristijan@gmail.com>
2022-05-14 09:26:00 +0200

    props correct

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,6 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-const expenseDate = new Date(2000, 0, 1);
-const expenseTitle = "Cat insurance";
-const expenseAmount = "10.50";
 
   return (
     <div className="expense-item">
