eacd5b89915ecc27caf06b692975a1bb2eba907f
Kristijan Federer <federer.kristijan@gmail.com>
2022-05-14 09:23:52 +0200

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+const expenseDate = new Date(2000, 0, 1);
+const expenseTitle = "Cat insurance";
+const expenseAmount = "10.50";
+
   return (
     <div className="expense-item">
-      <div>29th February 2022</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Cat Insurance</h2>
-        <div className="expense-item__price">10,50€</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}€</div>
       </div>
     </div>
   );
