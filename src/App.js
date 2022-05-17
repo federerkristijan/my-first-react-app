1837f39e8ba9981af0610421930cfbc33e70a260
Kristijan Federer <federer.kristijan@gmail.com>
2022-05-17 21:07:17 +0200

    2st assingment, bug found and fixed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import React from 'react';
 
-function App() {
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
-      id: "e1",
-      title: "Cat insurance",
-      amount: 10.5,
-      date: new Date(2000, 0, 1),
+      id: 'e1',
+      title: 'Cat food',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "Cat food", amount: 7.7, date: new Date(2000, 0, 2) },
+    { id: 'e2', title: 'Cat insurance', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: "e3",
-      title: "Dog insurance",
-      amount: 10.5,
-      date: new Date(2000, 0, 1),
+      id: 'e3',
+      title: 'Dog food',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id: "e4",
-      title: "Dog food",
-      amount: 11.5,
-      date: new Date(2000, 0, 2),
+      id: 'e4',
+      title: 'Dog insurance',
+      amount: 10.5,
+      date: new Date(2021, 5, 12),
     },
   ];
 
   const addExpenseHandler = expense => {
-    console.log("in App.js");
-    console.log("expense");
+    console.log('In App.js');
+    console.log(expense);
   };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,12 @@
-import React, { useState } from "module";
+import React, { useState } from "react";
 
-import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
-function Expenses(props) {
-  const [filteredYear, setFIlteredYear] = useState("2020");
+const Expenses = props => {
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
