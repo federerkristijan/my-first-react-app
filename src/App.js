68dd8e80eb8718f32c7279adcbb72dbacee7555a
Kristijan Federer <federer.kristijan@gmail.com>
2022-05-18 11:33:44 +0200

    rendered list of data

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   const expenses = [
     {
-      id: 'e1',
-      title: 'Cat food',
+      id: "e1",
+      title: "Cat food",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'Cat insurance', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Dog food',
+      id: "e2",
+      title: "Cat insurance",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Dog food",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'Dog insurance',
+      id: "e4",
+      title: "Dog insurance",
       amount: 10.5,
       date: new Date(2021, 5, 12),
     },
   ];
 
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
     console.log(expense);
   };
 
@@ -44,6 +49,6 @@ const App = () => {
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
